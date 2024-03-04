c457c02ad1a53752143f36bd681a20b8fbe1b967
Rahul Dubey <rahuldubay51@gmail.com>
2024-03-05 01:35:04 +0530

    second commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoURL =
+  "mongodb+srv://rahuldubay51:root@bookstore.rdkbfow.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore";
